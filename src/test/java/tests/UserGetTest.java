9e7b885383e7131bbabadcf7497610a21fcd8e96
kmaslikova <kmaslikova@t1-consulting.ru>
2023-05-23 21:52:56 +0300

    ex 16 add

---
+++ src/test/java/tests/UserGetTest.java
@@ -0,0 +1,70 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.ApiCoreRequests;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static lib.DataGenerator.getRegistrationData;
+
+public class UserGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+
+    @Test
+    @DisplayName("Ex16: Запрос данных другого пользователя")
+    public void testEx16() {
+        //Create user
+        String email = DataGenerator.getRandomEmail();
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+
+        userData =
+                getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
+        String createdUserId = responseCreateAuth.jsonPath().get("id");
+
+        //Auth another user for get headers and cookies for next request
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = apiCoreRequests
+                .makePostRequest(
+                        "https://playground.learnqa.ru/api/user/login",
+                        authData);
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+
+        //Get user info from another user
+        Response responseUserData = apiCoreRequests
+                .makeGetRequest(
+                        "https://playground.learnqa.ru/api/user/" + createdUserId,
+                        header,
+                        cookie);
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotKey(responseUserData, "firstName");
+        Assertions.assertJsonHasNotKey(responseUserData, "lastName");
+        Assertions.assertJsonHasNotKey(responseUserData, "email");
+    }
+}

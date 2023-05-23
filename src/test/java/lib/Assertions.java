e96fd859d120f12700ad2f847cf4d3a138117f76
kmaslikova <kmaslikova@t1-consulting.ru>
2023-05-23 21:03:01 +0300

    ex 15 add + add utils methods from lessons

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -3,10 +3,10 @@ package lib;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
-
 
     //add util methods from lecture
     public static void assertJsonByName(Response response, String name, int expectedValue) {
@@ -16,4 +16,53 @@ public class Assertions {
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
 
     }
+
+    //add util methods from lecture
+    public static void assertJsonByName(Response response, String name, String expectedValue) {
+        response.then().assertThat().body("$", hasKey(name));
+
+        String value = response.jsonPath().getString(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+
+    }
+
+    //add util methods from lecture
+    public static void assertResponseTextEquals(Response response, String expectedAnswer) {
+        assertEquals(
+                expectedAnswer,
+                response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    //add util methods from lecture
+    public static void assertResponseCodeEquals(Response response, int expectedStatusCode) {
+        assertEquals(
+                expectedStatusCode,
+                response.statusCode(),
+                "Response status code is not as expected"
+        );
+    }
+
+    //add util methods from lecture
+    public static void assertJsonHasField(Response response, String expectedFieldName) {
+        response.then().assertThat().body("$", hasKey(expectedFieldName));
+
+    }
+
+    //add util methods from lecture
+    public static void assertJsonHasFields(Response response, String[] expectedFieldNames) {
+        for (String expectedFieldName : expectedFieldNames) {
+            Assertions.assertJsonHasField(response, expectedFieldName);
+        }
+
+    }
+
+    //add util methods from lecture
+    public static void assertJsonHasNotKey(Response response, String unexpectedFieldName) {
+        response.then().assertThat().body("$", not(hasKey(unexpectedFieldName)));
+
+    }
+
+
 }
---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,111 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.ApiCoreRequests;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static lib.DataGenerator.getRegistrationData;
+
+public class UserRegisterTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+
+    @Test
+    @DisplayName("Ex15_1: Создание пользователя с некорректным email - без символа @")
+    @Description("Ex15: Тесты на метод user")
+    public void testEx15_1() {
+        String email = DataGenerator.getRandomEmail()
+                .replaceAll("@", "");
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+
+        userData =
+                getRegistrationData(userData);
+
+        Response responseCreateAuth =
+                apiCoreRequests
+                        .makePostRequest(
+                                "https://playground.learnqa.ru/api/user/",
+                                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"email", "password", "username", "firstName", "lastName"})
+    @DisplayName("Ex15_2: Создание пользователя без указания одного из полей")
+    @Description("Ex15: Тесты на метод user")
+    public void testEx15_2(String emptyField) {
+        String email;
+
+        Map<String, String> userData = new HashMap<>();
+
+        if (!emptyField.equals("email")) {
+            email = DataGenerator.getRandomEmail();
+            userData.put("email", email);
+            userData.put(emptyField, null);
+        } else {
+            userData.put("email", null);
+        }
+
+        userData =
+                getRegistrationData(userData);
+
+        Response responseCreateAuth =
+                apiCoreRequests
+                        .makePostRequest(
+                                "https://playground.learnqa.ru/api/user/",
+                                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: " + emptyField);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "g",
+            "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr. Sr.Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr. Sr.Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr." })
+    @DisplayName("Ex15_3/Ex15_4: Создание пользователя с очень коротким/длинным именем")
+    @Description("Ex15: Тесты на метод user")
+    public void testEx15_3(String username) {
+        String email = DataGenerator.getRandomEmail();
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+
+        userData.put("username", username);
+
+        userData =
+                getRegistrationData(userData);
+
+        Response responseCreateAuth =
+                apiCoreRequests
+                        .makePostRequest(
+                                "https://playground.learnqa.ru/api/user/",
+                                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        if (username.length() == 1) {
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too short");
+        }
+        if (username.length() > 250) {
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too long");
+        }
+
+    }
+
+}
